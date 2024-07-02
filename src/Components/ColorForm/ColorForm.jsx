d5bd5f880b2620a10d25b581aa164502e6f9ceff
Emma <emma.dirscherl@gmail.com>
2024-07-02 14:36:37 +0200

    add feature add color

--- src/Components/ColorForm/ColorForm.jsx
+++ src/Components/ColorForm/ColorForm.jsx
@@ -1,11 +1,23 @@
 import ColorInput from "../ColorInput/ColorInput";
 import "./ColorForm.css";
+import { uid } from "uid";
 
-export default function ColorForm() {
+export default function ColorForm({ onAddColor }) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newColor = {
+      id: uid(),
+      role: event.target.role.value,
+      hex: event.target.hex.value,
+      contrastText: event.target.contrastText.value,
+    };
+    onAddColor(newColor);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="role">Role</label>
-      <input id="role" name="role" type="text"></input>
+      <input id="role" name="role" type="text" placeholder="some color"></input>
       <ColorInput />
       <button type="submit">ADD COLOR</button>
     </form>
