5450431245de9e173e3539ccb6d961d03154181c
Emma <emma.dirscherl@gmail.com>
2024-07-02 15:11:19 +0200

    add function delete

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
 import "./Color.css";
 
-export default function Color({ color }) {
+export default function Color({ color, onConfirmDelete }) {
+  const [showConfirmation, setShowConfirmation] = useState(false);
+
+  const handleDeleteClick = () => {
+    setShowConfirmation(true);
+  };
+
+  const handleCancelClick = () => {
+    setShowConfirmation(false);
+  };
+
   return (
     <div
       className="color-card"
@@ -12,6 +23,17 @@ export default function Color({ color }) {
       <h3 className="color-card-headline">{color.hex}</h3>
       <h4>{color.role}</h4>
       <p>contrast: {color.contrastText}</p>
+      {!showConfirmation ? (
+        <button type="button" onClick={handleDeleteClick}>
+          DELETE
+        </button>
+      ) : (
+        <div>
+          <p>Really delete?</p>
+          <button onClick={handleCancelClick}>CANCEL</button>
+          <button onClick={() => onConfirmDelete(color.id)}>DELETE</button>
+        </div>
+      )}
     </div>
   );
 }
